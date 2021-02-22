4e3aa4ece380f534bc71c7470cd75fafa8b74666
arthursmel <arthurme@tcd.ie>
2021-02-22 15:21:19 +0000

    Commiting with an error in the project

--- app/src/main/java/rs/arthu/mel/androidpvt/MainActivity.kt
+++ app/src/main/java/rs/arthu/mel/androidpvt/MainActivity.kt
@@ -2,10 +2,13 @@ package rs.arthu.mel.androidpvt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "this will cause an error")
     }
 }
