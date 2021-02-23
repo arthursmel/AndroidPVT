c260e37dfccefcf8069a1a319b7dd39788464d40
arthursmel <arthurme@tcd.ie>
2021-02-23 14:10:31 +0000

    adding main activity

--- app/src/main/java/rs/arthu/mel/androidpvt/MainActivity.kt
+++ app/src/main/java/rs/arthu/mel/androidpvt/MainActivity.kt
@@ -1,11 +1,55 @@
 package rs.arthu.mel.androidpvt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import rs.arthu.mel.androidpvt.PvtActivity.Companion.NUMBER_OF_STIMULUS_KEY
+import rs.arthu.mel.androidpvt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonPvt.setOnClickListener {
+            startPvtActivity()
+        }
+    }
+
+    private fun startPvtActivity() {
+        val intent = Intent(this, PvtActivity::class.java)
+        intent.putExtra(NUMBER_OF_STIMULUS_KEY, 3)
+        startActivityForResult(intent, PVT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != RESULT_OK) return
+
+        val jsonResults: String? = when (requestCode) {
+            PVT_REQUEST -> {
+                data?.getStringExtra(PvtActivity.PVT_RESULTS_KEY)
+            }
+            else -> {
+                "No results"
+            }
+        }
+
+        Toast.makeText(this, jsonResults, Toast.LENGTH_LONG).show()
+        Log.d(TAG, jsonResults!!)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val PVT_REQUEST = 1
     }
 }
