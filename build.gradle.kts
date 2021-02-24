c77cf541193496a35515ddd86da9be085e0a29f9
arthursmel <arthurme@tcd.ie>
2021-02-24 19:35:38 +0000

    stuff for publishing on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,9 @@ buildscript {
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
+
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.10.2")
     }
 }
 
