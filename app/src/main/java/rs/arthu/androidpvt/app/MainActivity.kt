6dc7e7dc7c8464d173a3954584705aad4d1ef8d6
arthursmel <arthurme@tcd.ie>
2021-04-16 19:24:00 +0100

    updating gradle, corountines, junit

--- app/src/main/java/rs/arthu/androidpvt/app/MainActivity.kt
+++ app/src/main/java/rs/arthu/androidpvt/app/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
             startPvtActivity()
         }
     }
-
+    
     private fun startPvtActivity() {
         val pvtActivityIntent = PvtActivity.Builder()
             .withStimulusCount(3)
